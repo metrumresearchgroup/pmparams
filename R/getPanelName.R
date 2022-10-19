6b72f1935470ed6e18fe3c183ca4549e56092500
michaelmcd18 <michaelm@metrumrg.com>
2022-10-19 14:10:37 -0400

    getPanelName documentation/tests/requirements

--- R/getPanelName.R
+++ R/getPanelName.R
@@ -3,26 +3,31 @@
 #' @description
 #' Define which parameters should appear under which panel name in the final table.
 #'
+#' Panel types include:
+#' - Residual variance
+#' - Interindividual covariance parameters
+#' - Interindividual variance parameters
+#' - Interoccasion variance parameters
+#' - Covariate effect parameters
+#' - Structural model parameters
+#'
 #' @export
-getPanelName = function(df){
-  df %>%
-    mutate(type = case_when(panel=="RV" ~ "Residual variance",
-                            OM & !diag ~ "Interindividual covariance parameters",
-                            OM & diag & panel=="IIV" ~ "Interindividual variance parameters",
-                            # IOV not used here but included for convenience
-                            OM & diag & panel=="IOV"  ~ "Interoccasion variance parameters",
-                            panel=="cov" ~ "Covariate effect parameters",
-                            panel=="struct" ~ "Structural model parameters"),
-           # Make type a factor and use to sort, this ensures all parameters
-           # of the same type are together - needed to make sure panels pull out
-           # correct rows
-           type_f = case_when(panel=="RV" ~ 6,
-                              OM & !diag ~ 5,
-                              OM & diag & panel=="IIV" ~ 3,
-                              # IOV not used here but included for convenience
-                              OM & diag & panel=="IOV"  ~ 4,
-                              panel=="cov" ~ 2,
-                              panel=="struct" ~ 1)
+getPanelName = function(.df){
+  .df %>%
+    dplyr::mutate(
+      type = dplyr::case_when(panel=="RV" ~ "Residual variance",
+                              OM & !diag ~ "Interindividual covariance parameters",
+                              OM & diag & panel=="IIV" ~ "Interindividual variance parameters",
+                              OM & diag & panel=="IOV"  ~ "Interoccasion variance parameters",
+                              panel=="cov" ~ "Covariate effect parameters",
+                              panel=="struct" ~ "Structural model parameters"),
+
+      type_f = dplyr::case_when(panel=="RV" ~ 6,
+                         OM & !diag ~ 5,
+                         OM & diag & panel=="IIV" ~ 3,
+                         OM & diag & panel=="IOV"  ~ 4,
+                         panel=="cov" ~ 2,
+                         panel=="struct" ~ 1)
     ) %>%
-    arrange(type_f)
+    dplyr::arrange(type_f)
 }
