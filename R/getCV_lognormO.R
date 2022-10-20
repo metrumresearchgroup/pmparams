0356f8beda6ee5c2a09af2430cbac505aa938723
michaelmcd18 <michaelm@metrumrg.com>
2022-10-20 12:42:54 -0400

    add internal functions for getpCV

---
+++ R/getCV_lognormO.R
@@ -0,0 +1,13 @@
+#' Get lognorm CV ???
+#'
+#' @keywords internal
+getCV_lognormO  <- function(v){
+  sqrt(exp(v) - 1) * 100
+}
+
+
+
+
+
+
+
---
+++ R/getCV_propS.R
@@ -0,0 +1,6 @@
+#' Get CV proportion ???
+#'
+#' @keywords internal
+getCV_propS  <- function(v){
+  sqrt(v) * 100
+}
