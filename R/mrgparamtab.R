16c93dfee3e5589f757d97052e7188d4ff4dd2ac
graceo <graceo@metrumrg.com>
2023-01-25 17:04:33 -0500

    description file and pipe fix

---
+++ R/mrgparamtab.R
@@ -0,0 +1,16 @@
+#' @name mrgparamtab
+#' @title mrgparamtab
+#' @description `mrgparamtab` is a library written in R that generates clear,
+#' well-formatted parameter tables to report NONMEM® model results.
+#'
+#' @details
+#' `mrgparamtab` creates parameter tables by leveraging a few intuitive functions designed to:
+#'
+#'* Read in your model parameters from various NONMEM® output files
+#'* Describe your model parameters in a YAML file
+#'* Back-transform any parameters estimated in other domains (e.g., log- or logit-transformed variables)
+#'* Calculate additional summary statistics (e.g., 95% confidence intervals, coefficient of variation (CV), etc.)
+#'
+#' @importFrom magrittr %>%
+#' @keywords internal
+NULL
