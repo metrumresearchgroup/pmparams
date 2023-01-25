b8fb851fbbe088fb461aede55656626e37a833cb
graceo <graceo@metrumrg.com>
2023-01-25 17:37:29 -0500

    last round of getting rid of nonmem symbol

--- R/mrgparamtab.R
+++ R/mrgparamtab.R
@@ -9,7 +9,7 @@
 #'* Read in your model parameters from various NONMEM output files
 #'* Describe your model parameters in a YAML file
 #'* Back-transform any parameters estimated in other domains (e.g., log- or logit-transformed variables)
-#' Calculate additional summary statistics (e.g., 95% confidence intervals, coefficient of variation (CV), etc.)
+#'* Calculate additional summary statistics (e.g., 95% confidence intervals, coefficient of variation (CV), etc.)
 #'
 #' @importFrom magrittr %>%
 #' @keywords internal
