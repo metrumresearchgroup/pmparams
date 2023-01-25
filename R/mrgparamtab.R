686f9cdce826e7e0b83b3196d5d02047a197f8b2
graceo <graceo@metrumrg.com>
2023-01-25 17:19:24 -0500

    remove another nonmem symbol

--- R/mrgparamtab.R
+++ R/mrgparamtab.R
@@ -6,7 +6,7 @@
 #' @details
 #' `mrgparamtab` creates parameter tables by leveraging a few intuitive functions designed to:
 #'
-#'* Read in your model parameters from various NONMEM® output files
+#'* Read in your model parameters from various NONMEM output files
 #'* Describe your model parameters in a YAML file
 #'* Back-transform any parameters estimated in other domains (e.g., log- or logit-transformed variables)
 #' Calculate additional summary statistics (e.g., 95% confidence intervals, coefficient of variation (CV), etc.)
