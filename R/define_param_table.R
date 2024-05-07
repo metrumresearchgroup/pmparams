1933a437d011ab4ff42ac0d15a2f11db40164d66
graceo <graceo@metrumrg.com>
2024-05-07 11:53:26 -0400

    Update per QC comment

--- R/define_param_table.R
+++ R/define_param_table.R
@@ -57,7 +57,7 @@ define_param_table <- function(.estimates, .key, .ci = 95, .zscore = NULL){
     dplyr::arrange(as.numeric(nrow))
 
   if(any(mod_estimates$THETAERR)){
-    message("THETA term was used in $ERROR:  ", paste(mod_estimates$parameter_names[mod_estimates$THETAERR]))
+    message("THETA term was used in $ERROR:  ", paste(mod_estimates$parameter_names[mod_estimates$THETAERR], collapse = ", "))
   }
 
   return(mod_estimates)
