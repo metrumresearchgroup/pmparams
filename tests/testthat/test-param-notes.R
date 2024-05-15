8b47f194aa54cf0d1602f6be7d50dcef2f41ee8d
graceo <graceo@metrumrg.com>
2024-05-15 18:37:56 -0400

    ci and zscore update

--- tests/testthat/test-param-notes.R
+++ tests/testthat/test-param-notes.R
@@ -6,13 +6,13 @@ test_that("param_notes unexpected input: confidence internal that is not 90 or 9
 
 test_that("param_notes unexpected input: confidence internal and z-score mismatch",{
   eq1 <- param_notes(.ci = 95, .zscore = 1.9)
-  expect_equal(eq1$ciEq, paste0("Confidence intervals = estimate $\\pm$ ", 1.64, " $\\cdot$ SE"))
+  expect_equal(eq1$ciEq, paste0("Confidence intervals = estimate $\\pm$ ", 1.96, " $\\cdot$ SE"))
   expect_message(capture.output(param_notes(.ci = 95, .zscore = 1.9)))
 })
 
 test_that("param_notes expected output: z-score",{
   eq2 <- param_notes(.ci = 95)
-  expect_equal(eq2$ciEq, paste0("Confidence intervals = estimate $\\pm$ ", 1.64, " $\\cdot$ SE"))
+  expect_equal(eq2$ciEq, paste0("Confidence intervals = estimate $\\pm$ ", 1.96, " $\\cdot$ SE"))
 
   z = 1.4
   eq3 <- param_notes(.ci = 5, .zscore = z)
