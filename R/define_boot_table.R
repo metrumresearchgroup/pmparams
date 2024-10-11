87cbb32e63580b86d6b5679044490fbaabc71fbe
Kyle Barrett <barrettk@metrumrg.com>
2024-10-11 12:11:42 -0400

    update define_param_table and define_boot_table
     - Documentation adjustments, especially for `define_boot_table`
     - Make examples more clear and illustrate various scenarios of specifying `.estimates` or `.boot_estimates`
     - Some code cleanup

--- R/define_boot_table.R
+++ R/define_boot_table.R
@@ -2,79 +2,109 @@
 #'
 #' @description
 #'
-#' Combines boot strap estimates and non boot strap estimates with information in parameter key. Performs
-#' some formatting of this combined data.frame.
-#' There are two main steps of this function:
+#' Combines bootstrap estimates with information in parameter key and performs
+#' some additional formatting.
+#'
+#' @details
+#'
+#' Below is the expected format of `.boot_estimates` if a data frame is provided:
+#'    ```
+#'    #>   run   THETA1 THETA2 THETA3 THETA4 THETA5 THETA6   THETA7 THETA8
+#'    #>   <chr>  <dbl>  <dbl>  <dbl>  <dbl>  <dbl>  <dbl>    <dbl>  <dbl>
+#'    #>   001    0.494   4.19   1.20   4.20   1.24  0.488 -0.0615   0.377
+#'    #>   002    0.405   4.07   1.14   4.23   1.32  0.536 -0.103    0.517
+#'    #>   003    0.500   4.09   1.15   4.21   1.27  0.413 -0.0752   0.523
+#'    #>   004    0.578   4.16   1.20   4.18   1.30  0.518 -0.0502   0.418
+#'    #>   005    0.499   4.14   1.16   4.25   1.22  0.436 -0.0686   0.394
+#'    ```
+#'
+#' **Notes:**
+#' - Some parameter names may have punctuation (such as `OMEGA(1,1)`). A new
+#' `name` column is automatically added that specifies the name without
+#'  punctuation (i.e. `OMEGA11`).
+#'
+#' - Parameter details from the parameter key are joined to the bootstrap parameter
+#' estimates. A `dplyr::inner_join` is used so that only parameters in the model
+#' output are kept in the table. This was done so that, if your base and final
+#' model used the same structural THETAs and random parameters, the same
+#' parameter key could be used for both.
+#'   - This join adds the following columns: `abb` (abbreviation), `desc`
+#'     (parameter description), `panel`, `trans` (transformation).
+#'
+#' - A final check is performed to determine whether parameters with special
+#' transformation rules were defined correctly. In addition, a series of `TRUE`/
+#' `FALSE` columns are added that will be used downstream.
 #'
 #'
-#'1.Run `bbr::param_estimates_compare` to extract summary quantiles, the 5th, 50th, and 95th, of the
-#' bootstrap estimates for each model parameter.
-#'
-#' - Some `parameter_names` have punctuation such as `OMEGA(1,1)`. A new column is
-#' added without punctuation, such as `OMEGA11`.
-#'
-#' - Following this, parameter details from the parameter key are joined to the boot strap parameter estimates.
-#' A `dplyr::inner_join` is used so that only parameters in the model output are kept
-#' in the table. This was done so that, if your base and final model used the same structural
-#' THETAs and random parameters, the same parameter key could be used for both.
-#'
-#' - This join adds the following columns: `abb` (abbreviation), `desc` (parameter description),
-#' `panel`, `trans` (transformation).
-#'
-#' With this information provided, a check is performed to determine whether parameters
-#' with special transformation rules were defined correctly. In addition, a series of
-#' TRUE/FALSE columns that will be used by subsequent functions.
-#'
-#' @param .boot_estimates parameter boot estimates- either path to file or data.frame
-#' @param .key path to parameter key or data.frame of parameter key. Described in more detail in \code{\link[pmparams]{param_key}}
-#' @param .ci specify 90 or 95 percent confidence interval or IQR (interquartile range). Default is 95.
-#' @param .percentiles list of all percentiles. Default is NULL. For .ci = 95, .percentiles = c(0.025, 0.5, 0.975). For .ci =90, .percentiles will be c(0.05, 0.5, 0.95)
-#' @param .na.rm Default is false
+#' @param .boot_estimates One of the following:
+#' - Output from `bbr::bootstrap_estimates()` or a wide data frame denoting the
+#' parameter estimates for each run. See details.
+#' - A file path to a csv containing the above dataset.
+#' @param .key path to parameter key or data.frame of parameter key. Described
+#'   in more detail in \code{\link[pmparams]{param_key}}
+#' @param .ci specify 90 or 95 percent confidence interval or IQR (interquartile
+#'   range). Default is 95.
+#' @param .percentiles list of all percentiles. Default is NULL. For .ci = 95,
+#'   .percentiles = c(0.025, 0.5, 0.975). For .ci =90, .percentiles will be
+#'   c(0.05, 0.5, 0.95)
+#' @param .na.rm Logical (T/F). If `TRUE`, any `NA` and `NaN`'s are removed
+#'  before the quantiles are computed.
 #' @examples
 #'
-#' boot_paramEst <- utils::read.csv(system.file("model/nonmem/boot/data/boot-106.csv",
-#'                                  package = "pmparams"))
-#' paramKey <-  system.file("model/nonmem/pk-parameter-key-new.yaml", package = "pmparams")
+#' model_dir <- system.file("model/nonmem", package = "pmparams")
+#' paramKey <-  file.path(model_dir, "pk-parameter-key-new.yaml")
 #'
-#' define_boot_table(.boot_estimates = boot_paramEst,
-#'                .key = paramKey)
+#' # Using `bbr::bootstrap_estimates()`
+#' boot_run <- bbr::read_model(file.path(model_dir, "106-boot"))
+#' define_boot_table(
+#'  .boot_estimates = bbr::bootstrap_estimates(boot_run),
+#'  .key = paramKey
+#' )
 #'
+#' # Using a file path
+#' boot_path <- file.path(model_dir, "boot/data/boot-106.csv")
+#' define_boot_table(
+#'  .boot_estimates = boot_path,
+#'  .key = paramKey
+#' )
+#'
+#' @seealso [param_key()], [define_param_table()]
 #' @export
-define_boot_table <- function(.boot_estimates, .key, .ci = 95, .percentiles = NULL,
-                              .na.rm = TRUE, .nonboot_estimates = NULL){
+define_boot_table <- function(
+    .boot_estimates,
+    .key,
+    .ci = 95,
+    .percentiles = NULL,
+    .na.rm = TRUE
+){
 
-
-  if (length(.nonboot_estimates)>0){
-    message("Non-boot estimates no longer joined in this step.")
-  }
-
-  #path to boot estimates
+  # path to boot estimates
   if (inherits(.boot_estimates, "character")){
     .boot <- readr::read_csv(.boot_estimates, show_col_types = FALSE)
-  #data.frame of boot estimates
+    # data.frame of boot estimates
   } else {
     .boot <- .boot_estimates
   }
 
-# parameter key types
+  # parameter key types
   .key <- loadParamKey(.key)
 
-#clean up boot
+  # clean up boot
   .bootParam0 = .boot %>%
     getBootPercentiles(.ci, .percentiles, .na.rm)
 
-.bootParam <- .bootParam0 %>%
+  .bootParam <- .bootParam0 %>%
     dplyr::mutate(name = gsub("[[:punct:]]", "", parameter_names)) %>%
     dplyr::inner_join(.key, by = "name")
 
-#join with nonboot estimates
-.boot_df <- .bootParam %>%
-  checkTransforms() %>%
-  defineRows() %>%
-  backTrans_log() %>%
-  backTrans_logit() %>%
-  dplyr::arrange(as.numeric(nrow))
+  # join with nonboot estimates
+  .boot_df <- .bootParam %>%
+    checkTransforms() %>%
+    defineRows() %>%
+    backTrans_log() %>%
+    backTrans_logit() %>%
+    dplyr::arrange(as.numeric(nrow))
 
-.boot_df
-
+  return(.boot_df)
 }
+
--- R/define_param_table.R
+++ R/define_param_table.R
@@ -5,41 +5,77 @@
 #' Combines model output parameter estimates with information in parameter key. Performs
 #' some formatting of this combined data.frame.
 #'
-#' Expected input is a data.frame with parameter estimates, with the columns:
-#' `parameter_names`, `estimate`, `stderr`, `random_effect_sd`, `random_effect_sdse`,
-#' `fixed`, `diag`, `shrinkage`.
+#' @details
 #'
-#' Some `parameter_names` have punctuation such as `OMEGA(1,1)`. A new column is
-#' added without punctuation, such as `OMEGA11`.
+#' Below is the expected format of `.estimates` if a data frame is provided:
+#'    ```
+#'    parameter_names estimate  stderr random_effect_sd random_effect_sdse fixed diag  shrinkage
+#'    <chr>              <dbl>   <dbl>            <dbl>              <dbl> <lgl> <lgl>     <dbl>
+#'    THETA1            0.443  0.0643            NA               NA       FALSE NA       NA
+#'    THETA2            4.12   0.0275            NA               NA       FALSE NA       NA
+#'    THETA3            1.17   0.0280            NA               NA       FALSE NA       NA
+#'    THETA4            4.21   0.0190            NA               NA       FALSE NA       NA
+#'    THETA5            1.28   0.0348            NA               NA       FALSE NA       NA
+#'    THETA6            0.485  0.0395            NA               NA       FALSE NA       NA
+#'    THETA7           -0.0378 0.0635            NA               NA       FALSE NA       NA
+#'    THETA8            0.419  0.0863            NA               NA       FALSE NA       NA
+#'    ```
 #'
-#' Following this, parameter details from the parameter key are joined to the parameter estimates.
-#' A `dplyr::inner_join` is used so that only parameters in the model output are kept
-#' in the table. This was done so that, if your base and final model used the same structural
-#' THETAs and random parameters, the same parameter key could be used for both.
+#' **Notes:**
+#' - Some parameter names may have punctuation (such as `OMEGA(1,1)`). A new
+#' `name` column is automatically added that specifies the name without
+#'  punctuation (i.e. `OMEGA11`).
 #'
-#' This join adds the following columns: `abb` (abbreviation), `desc` (parameter description),
-#' `panel`, `trans` (transformation).
+#' - Parameter details from the parameter key are joined to the bootstrap parameter
+#' estimates. A `dplyr::inner_join` is used so that only parameters in the model
+#' output are kept in the table. This was done so that, if your base and final
+#' model used the same structural THETAs and random parameters, the same
+#' parameter key could be used for both.
+#'   - This join adds the following columns: `abb` (abbreviation), `desc`
+#'     (parameter description), `panel`, `trans` (transformation).
 #'
-#' With this information provided, a check is performed to determine whether parameters
-#' with special transformation rules were defined correctly. In addition, a series of
-#' TRUE/FALSE columns that will be used by subsequent functions.
+#' - A final check is performed to determine whether parameters with special
+#' transformation rules were defined correctly. In addition, a series of `TRUE`/
+#' `FALSE` columns are added that will be used downstream.
 #'
-#' @param .estimates path to model directory, bbr NONMEM model, or data.frame of parameter estimates
-#' @param .key path to parameter key or data.frame of parameter key. Described in more detail in \code{\link[pmparams]{param_key}}
+#' @inheritParams loadParamEstimates
+#' @param .key path to parameter key or data.frame of parameter key. Described
+#'   in more detail in \code{\link[pmparams]{param_key}}
 #' @param .ci confidence interval. Default is 95
-#' @param .zscore z-score for the specified confidence interval. Only needed for confidence intervals that are NOT 90 or 95 percent
+#' @param .zscore z-score for the specified confidence interval. Only needed for
+#'   confidence intervals that are **not** 90 or 95 percent
 #'
-#' @seealso [param_key()]
 #' @examples
 #'
-#' paramEst <- utils::read.csv(system.file("model/nonmem/param_est.csv", package = "pmparams"))
-#' paramKey <-  system.file("model/nonmem/pk-parameter-key-new.yaml", package = "pmparams")
+#' model_dir <- system.file("model/nonmem", package = "pmparams")
+#' paramKey <-  file.path(model_dir, "pk-parameter-key-new.yaml")
 #'
-#' define_param_table(.estimates = paramEst, .key = paramKey, .ci = 95, .zscore = NULL)
+#' # Using a `bbr` model
+#' mod <- bbr::read_model(file.path(model_dir, "106"))
+#' define_param_table(
+#'  .estimates = mod,
+#'  .key = paramKey,
+#'  .ci = 95,
+#' )
 #'
-#' #To choose a confidence interval that is not 95 or 90, look up z-score and add as function parameter
-#' define_param_table(.estimates = paramEst, .key = paramKey, .ci = 82, .zscore = 0.915)
+#' # Using a file path
+#' param_est_path <- file.path(model_dir, "param_est.csv")
+#' param_ests <- readr::read_csv(param_est_path)
+#' define_param_table(
+#'  .estimates = param_ests,
+#'  .key = paramKey,
+#'  .ci = 95,
+#' )
 #'
+#' # To choose a confidence interval that is not 95 or 90, look up and provide the z-score
+#' define_param_table(
+#'  .estimates = param_ests,
+#'  .key = paramKey,
+#'  .ci = 82,
+#'  .zscore = 0.915
+#' )
+#'
+#' @seealso [param_key()], [define_boot_table()]
 #' @export
 define_param_table <- function(.estimates, .key, .ci = 95, .zscore = NULL){
 
