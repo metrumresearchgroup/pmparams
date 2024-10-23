69a5c766aa38bc473cfbf3c9f260eaa35317f85a
Kyle Barrett <barrettk@metrumrg.com>
2024-10-23 15:19:19 -0400

    Remove "iqr" from supported `param_notes`

--- tests/testthat/test-param-notes.R
+++ tests/testthat/test-param-notes.R
@@ -24,7 +24,7 @@ test_that("param_notes expected output: z-score",{
 
 test_that("param_notes expected output: footnotes",{
   eq4 <- param_notes()
-  expect_equal(12, length(eq4))
+  expect_equal(11, length(eq4))
   expect_equal(anyNA(eq4), FALSE)
   expect_equal(eq4$cvOmegaEq,"CV\\% of log-normal omegas = sqrt(exp(estimate) - 1) $\\cdot$ 100")
 })
