d2e8c63c9a625683cad731ae6fba5459157045de
Kyle Barrett <barrettk@metrumrg.com>
2025-02-11 13:00:16 -0500

    update grammar in param_notes
     - changes are consistent with those requested in #82

--- tests/testthat/test-param-notes.R
+++ tests/testthat/test-param-notes.R
@@ -20,5 +20,5 @@ test_that("param_notes expected output: footnotes",{
   eq4 <- param_notes()
   expect_equal(11, length(eq4))
   expect_equal(anyNA(eq4), FALSE)
-  expect_equal(eq4$cvOmegaEq,"CV\\% of log-normal omegas = sqrt(exp(estimate) - 1) $\\cdot$ 100")
+  expect_equal(eq4$cvOmegaEq,"CV\\% of log-normal omega = sqrt(exp(estimate) - 1) $\\cdot$ 100")
 })
