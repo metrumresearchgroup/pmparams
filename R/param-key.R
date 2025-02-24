f0df1b7c9ecc018138d70b50178a862a119f2242
Kyle Barrett <barrettk@metrumrg.com>
2025-02-24 11:41:11 -0500

    param_key: format NONMEM records and functions as code

--- R/param-key.R
+++ R/param-key.R
@@ -15,23 +15,23 @@
 #'
 #' Current options include:
 #'
-#'   - panel=="struct" ~ "Structural model parameters"
-#'   - panel=="cov" ~ "Covariate effect parameters"
-#'   - panel=="IIV" ~ "Interindividual covariance parameters" (off-diagonals << function takes care of this)
-#'   - panel=="IIV" ~ "Interindividual variance parameters" (diagonals << function takes care of this)
-#'   - panel=="IOV"  ~ "Interoccasion variance parameters"
-#'   - panel=="RV" ~ "Residual variance"
+#'   - `panel=="struct"` ~ "Structural model parameters"
+#'   - `panel=="cov"` ~ "Covariate effect parameters"
+#'   - `panel=="IIV"` ~ "Interindividual covariance parameters" (off-diagonals << function takes care of this)
+#'   - `panel=="IIV"` ~ "Interindividual variance parameters" (diagonals << function takes care of this)
+#'   - `panel=="IOV"`  ~ "Interoccasion variance parameters"
+#'   - `panel=="RV"` ~ "Residual variance"
 #'
 #' __trans__ = define how the parameter will be transformed. Current options include:
-#'   - "none" -   untransformed parameters, e.g. THETAs or off-diagonals
-#'   - "logTrans" -  THETAs estimated in the log domain
-#'   - "logitTrans" - THETAs estimated using a logit transform
-#'   - "lognormalOm"- for log-normal omegas e.g. CL = THETA(1) * exp(ETA(1)) - returns est.+CV%
-#'   - "OmSD"       - for omegas where you want to return SD only - returns estimate & SD; use this for additive omegas e.g. CL = THETA(1) + ETA(1)
-#'   - "logitOmSD"  - for omegas using logit transform - returns estimate & SD (calculated with logitnorm package); this option requires you provide the associated THETA separated with a "~"; e.g. "logitOmSD ~ THETA3"
-#'   - "addErr"     - for additive error terms (coded using THETA or SIGMA in $ERROR) - returns est.+ SD
-#'   - "propErr"    - for proportional error terms (coded using THETA or SIGMA in $ERROR) - returns est.+CV%
-#'   - "addErrLogDV" - for an additive error term on log-transformed data (coded using THETA or SIGMA in $ERROR) - returns est.+CV%
+#'   - `"none"` -   untransformed parameters, e.g. THETAs or off-diagonals
+#'   - `"logTrans"` -  THETAs estimated in the log domain
+#'   - `"logitTrans"` - THETAs estimated using a logit transform
+#'   - `"lognormalOm"`- for log-normal omegas e.g. `CL = THETA(1) * exp(ETA(1))` - returns est.+CV%
+#'   - `"OmSD"`       - for omegas where you want to return SD only - returns estimate & SD; use this for additive omegas e.g. `CL = THETA(1) + ETA(1)`
+#'   - `"logitOmSD"`  - for omegas using logit transform - returns estimate & SD (calculated with `logitnorm` package); this option requires you provide the associated `THETA` separated with a "~"; e.g. `"logitOmSD ~ THETA3"`
+#'   - `"addErr"`     - for additive error terms (coded using `THETA` or `SIGMA` in `$ERROR`) - returns est.+ SD
+#'   - `"propErr"`    - for proportional error terms (coded using `THETA` or `SIGMA` in `$ERROR`) - returns est.+CV%
+#'   - `"addErrLogDV"` - for an additive error term on log-transformed data (coded using `THETA` or `SIGMA` in `$ERROR`) - returns est.+CV%
 #'
 #' # YAML Example
 #'
